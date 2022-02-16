87f4469ae99263db8302410ab69155cff54c9fb9
Ravi3599 <ravikantdukre2018@gmail.com>
2022-02-16 14:55:36 +0530

    UPDATED: displayed message by using rest call

---
+++ src/main/java/com/bridgelabz/springdemo/controller/HelloRestController.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.springdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/hello")
+public class HelloRestController {
+
+	@RequestMapping(value={"", "/", "/home"})
+	public String sayHello() {
+	return "Hello From BridgeLabz!";
+	}
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHelloParam1 (@RequestParam(value="name")String name) {
+		return "Hello " + name + "!";
+	}
+	@GetMapping ("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+	return "Hello "+ name+ "!";
+	}
+}
